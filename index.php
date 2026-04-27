de96d6542e1f52dd47a82ff7c84b7d0e26295695
ezer <ezerayarii@gmail.com>
2026-04-26 22:29:32 -0700

    Initial commit - Week 2 project

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First Web Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header>
+        <h1>Welcome to My Website</h1>
+    </header>
+<img src="https://tse2.mm.bing.net/th/id/OIP.ac8SS3sITKocmuoR_TWCNwAAAA?pid=Api&h=220&P=0" alt="Sample Image">
+    <section>
+        <h2>About Me</h2>
+        <p>Hello, my name is Ezer. This is my first web project.</p>
+        <button>Contact Me</button>
+    </section>
+
+    <section>
+        <h2>My Skills</h2>
+        <nav>
+
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Web Design</li>
+        </ul>
+        </nav>
+    </section>
+
+    <footer>
+        <p>© 2026 Ezer Ayari</p>
+    </footer>
+
+</body>
+</html>
