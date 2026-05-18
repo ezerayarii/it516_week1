3ca270a0651fcc20c0b1864e80cb80182adcfa2c
ezer <ezerayarii@gmail.com>
2026-05-18 01:12:02 -0700

    fix page route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main>
+    <main style={{ padding: "20px" }}>
       <h1>Welcome to My Website</h1>
+      <p>Hello, my name is Ezer. This is my React project.</p>
     </main>
   );
 }
